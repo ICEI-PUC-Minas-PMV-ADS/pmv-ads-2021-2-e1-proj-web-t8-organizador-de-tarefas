4d0c5a8d843aec5eb88d0b671239628af111de5d
Isabella Fernanda Campos Emiliano <isabellaemiliano14@gmail.com>
2021-11-26 19:21:44 -0300

    Novas funcionalidades na tela de tarefas
    + Adicionando e removendo tarefas e salvando no localStorage;
    
    + Compartilhando tela de tarefas, porém modal precisa do CSS

--- Gerenciador de Tarefas/assets/js/tarefas.js
+++ Gerenciador de Tarefas/assets/js/tarefas.js
@@ -6,33 +6,84 @@ function fechaModal(){
     document.getElementById('modal-task').style.top = '-100%';
 }
 
+showLista()
+
 document.querySelector('#addTask').onclick = function(){
+   
     if(document.querySelector('#newTask').value.length == 0){
         alert("Adicione uma tarefa!")
     }else{
-        document.querySelector('#table-task').innerHTML 
-        += `
+        let statusTask = document.getElementById('status-task').value
+        console.log(statusTask)
+
+        let dataTask = document.getElementById('data-task').value
+        console.log(dataTask)
+        if(document.querySelector('#newTask').value != 0){
+            let localItens = JSON.parse(localStorage.getItem('localItem'))
+            if(localItens === null){
+                tasks = []
+            }else{
+                tasks = localItens;
+                localStorage.setItem('localItem', "[]")
+            }
+
+            let infoTask = {
+                nome: document.querySelector('#newTask').value,
+                status: statusTask,
+                data: dataTask
+            }
+            tasks.push(infoTask)
+            localStorage.setItem('localItem', JSON.stringify(tasks))
+        }   
+        showLista()
+        fechaModal()
+    }
+}
+
+function showLista(){
+    let localItens = JSON.parse(localStorage.getItem('localItem'))
+    let mostraLista = document.querySelector('#table-task')
+    let output = '';
+    if(localItens === null){
+        tasks = []
+    }else{
+        tasks = localItens;
+    }    
+    tasks.forEach((data, index) => {
+        output += `
             <tr>
                 <td>
-                    ${document.querySelector('#newTask').value}
+                    ${data.nome}
                 </td>
                 <td>
-                    ${document.querySelector('#status-task').value}
+                    ${data.status}
                 </td>
                 <td>
-                    <img class="delete-task" src="assets/img/trash.png">
+                    ${data.data}
                 </td>
                 <td>
-                <img onclick="showModal()" src="assets/img/write (1).png">
-            </td>
+                    <img onclick="deleteTask(${index})" class="delete-task" src="assets/img/trash.png">
+                </td>
             </tr>
-            `;
-        fechaModal()
-        // let taskAtual = document.querySelectorAll('.delete-task');
-        // for(i=0;i<taskAtual.length;i++){
-        //     taskAtual[i].onclick = function(){
-        //         this.parentNode.remove();
-        //     }
-        // }
+        `;
+    });
+    mostraLista.innerHTML = output;    
+}
+
+function deleteTask(index){
+    let localItens = JSON.parse(localStorage.getItem('localItem'))
+    tasks.splice(index,1)
+    localStorage.setItem('localItem', JSON.stringify(tasks))
+    showLista()
+}
+
+function compartilharToDoList(){
+    document.querySelector('#share').onclick = function(){
+        document.getElementById('div-compartilhar').style.display ='block';
+        let link = window.location.href
+
+        document.getElementById('compartilhar').value = link;
+        document.getElementById('compartilhar').select();
+        document.execCommand('copy')
     }
 }
